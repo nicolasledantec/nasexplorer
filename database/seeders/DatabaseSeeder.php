007dbeac8979060d78423b7ab80b79c425899c73
NicoLD <gorky17@hotmail.fr>
2022-11-12 14:52:22 +0100

    Fnc : Ajout du modèle user

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      // \App\Models\User::factory(10)->create();
+
+       User::factory()->create([
+         'login' => 'admin',
+         'nom' => 'admin',
+         'password' => bcrypt('admin'),
+         "admin" => true,
+       ]);
+    }
+}
